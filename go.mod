2e9c14a35878dacd1c05f958196413c18b465bb8
Stepa-Nos <nosotepa@yandex.ru>
2022-08-16 15:04:52 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/Stepa-Nos/CRM-Go-Fiber
+
+go 1.16
+
+require (
+	github.com/gofiber/fiber v1.14.6
+	github.com/gofiber/utils v0.1.2 // indirect
+	github.com/gorilla/schema v1.2.0 // indirect
+	github.com/jinzhu/gorm v1.9.16
+	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/valyala/fasthttp v1.38.0 // indirect
+	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
+)
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Stepa-Nos/CRM-Go-Fiber/database"
+	"github.com/Stepa-Nos/CRM-Go-Fiber/lead"
+
+	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/api/v1/lead", lead.GetLeads)
+	app.Get("/api/v1/lead/:id", lead.GetLead)
+	app.Post("/api/v1/lead", lead.NewLead)
+	app.Delete("/api/v1/lead/:id", lead.DeleteLead)
+}
+
+func initDatabase() {
+	var err error
+	database.DBConn, err = gorm.Open("sqlite3", "leads.db")
+	if err != nil {
+		panic("failed to connect DB")
+	}
+	fmt.Print("Connection opend to DB")
+	database.DBConn.AutoMigrate(&lead.Lead{})
+	fmt.Println("Database migrated")
+}
+
+func main() {
+	app := fiber.New()
+	initDatabase()
+	setupRoutes(app)
+	app.Listen(3000)
+	defer database.DBConn.Close()
+}
